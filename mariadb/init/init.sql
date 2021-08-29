6ace59076732d971bd7ae90ac587650d5ed6c5cf
mikaahopelto <69237295+mikaahopelto@users.noreply.github.com>
2021-08-29 20:39:27 +0300

    Moved the passwords to env variables

--- mariadb/init/init.sql
+++
@@ -1,5 +0,0 @@
-SET PASSWORD FOR 'root' = PASSWORD('changethis');
-FLUSH PRIVILEGES;
-CREATE USER 'CabinUser'@'localhost' IDENTIFIED BY 'changethis';
-FLUSH PRIVILEGES;
-GRANT ALL PRIVILEGES ON `Cabin_%` . * TO 'CabinUser'@'localhost';
