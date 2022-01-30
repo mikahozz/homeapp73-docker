8b8c22a349003108746c13d08a1b9b2ce516d36f
mikaahopelto <69237295+mikaahopelto@users.noreply.github.com>
2022-01-30 15:14:59 +0200

    First working version of the api with correct prices and datetimes

--- nordpool/index.js
+++ nordpool/index.js
@@ -16,15 +16,21 @@ app.get('/test', (req, res) => {
 
         let settings = { method: "Get" };
         
+        console.log("Calling nordpool: " + url);
         fetch(url, settings)
             .then(res => res.json())
             .then((json) => {
-                let jsonRes = JSON.stringify(json);
+                const prices = getPriceArray(json.data.Rows, 1).concat(getPriceArray(json.data.Rows));
+                const jsonRes = JSON.stringify(prices);
                 res.writeHead(200, {"Content-Type": "application/json"});
                 res.write(jsonRes);
                 res.end();
-            });
-    } catch (err) {
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).send(err.message);
+            })
+        } catch (err) {
         console.error(err);
         res.status(500).send(err.message);
     }
@@ -39,3 +45,19 @@ process.on('SIGINT', () => {
   console.info("Interrupted")
   process.exit(0)
 })
+
+function getPriceArray(dataRows, daysAgo = 0) {
+    return dataRows.filter((rows) => (rows.IsExtraRow == false))
+        .map((row) => {
+            return { 
+                DateTime: parseDate(row.Columns[daysAgo].Name, row.StartTime ), 
+                Price: (parseFloat(row.Columns[daysAgo].Value.replace(',', '.')) / 10 * 1.24 + 0.24).toFixed(3)};
+        });
+}
+function parseDate(dateCET, timeLocal) {
+    const dateStr = dateCET.split('-').reverse().join('-');
+    const timeStr = timeLocal.slice(11,19);
+    let date = new Date(dateStr + "T" + timeStr + "Z");
+    date.setHours(date.getHours() - 1); // Reduce one our since the time was in CET and we created a UTC date
+    return date;
+}
