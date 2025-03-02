dc722ebb7e30231ba4eea4bbc2224a347030088f
Mika Ahopelto <mika.ahopelto@gmail.com>
2025-03-02 17:45:08 +0200

    Added /api to the routes

--- climateapi/indoor.py
+++ climateapi/indoor.py
@@ -4,26 +4,28 @@ import datetime as dt
 from flask import jsonify
 import utils
 
-@app.route("/indoor/<device>")
+
+@app.route("/api/indoor/<device>")
 def indoor(device):
-    client = InfluxDBClient(host='influxdb', port=8086)
-    client.switch_database('homedb')
+    client = InfluxDBClient(host="influxdb", port=8086)
+    client.switch_database("homedb")
     results = client.query(f"SELECT LAST(temperature),* FROM indoorclimate where location = '{device}'")
 
     resultgen = results.get_points()
     response = {}
     for value in resultgen:
         print(x for x in value.keys())
-        date = utils.parseDate(value['time'])
-        temperature = value['temperature']
-        humidity = value['humidity']
-        battery = value['battery']
-        response = { 'time': value['time'],
-            'temperature': value['temperature'], 
-            'humidity': value['humidity'],
-            'battery': value['battery']
-            }
-        print('Response ', response)
+        date = utils.parseDate(value["time"])
+        temperature = value["temperature"]
+        humidity = value["humidity"]
+        battery = value["battery"]
+        response = {
+            "time": value["time"],
+            "temperature": value["temperature"],
+            "humidity": value["humidity"],
+            "battery": value["battery"],
+        }
+        print("Response ", response)
         break
 
     return jsonify(response)
--- climateapi/outdoor.py
+++ climateapi/outdoor.py
@@ -4,66 +4,82 @@ import datetime as dt
 import pandas as pd
 from flask import Flask, request, jsonify, Response
 
-@app.route("/outdoor")
+
+@app.route("/api/outdoor")
 def outdoor():
-    location = request.args.get('location')
+    location = request.args.get("location")
 
     # Handle date inputs
-    if('start_date' in request.args):
+    if "start_date" in request.args:
         try:
-            startTime = dt.datetime.strptime(request.args.get('start_date'), "%Y-%m-%dT%H:%M:%SZ")
+            startTime = dt.datetime.strptime(request.args.get("start_date"), "%Y-%m-%dT%H:%M:%SZ")
         except ValueError:
-            return 'Invalid start_date. Should be yyyy-MM-ddThh:mm:ssZ', 400
-        if(startTime > dt.datetime.utcnow()):
-            return 'start_date cannot be in future', 400
-        if('end_date' in request.args):
+            return "Invalid start_date. Should be yyyy-MM-ddThh:mm:ssZ", 400
+        if startTime > dt.datetime.utcnow():
+            return "start_date cannot be in future", 400
+        if "end_date" in request.args:
             try:
-                endTime = dt.datetime.strptime(request.args.get('end_date'), "%Y-%m-%dT%H:%M:%SZ")
+                endTime = dt.datetime.strptime(request.args.get("end_date"), "%Y-%m-%dT%H:%M:%SZ")
             except ValueError:
                 return "Invalid end_date. Should be yyyy-MM-ddThh:mm:ssZ", 400
-            if(startTime > endTime):
-                return 'start_date cannot be greater than end_date', 400
+            if startTime > endTime:
+                return "start_date cannot be greater than end_date", 400
         else:
-            endTime = startTime + dt.timedelta(minutes=(6*24*60)-10)
+            endTime = startTime + dt.timedelta(minutes=(6 * 24 * 60) - 10)
     else:
-        if('end_date' in request.args):
+        if "end_date" in request.args:
             try:
-                endTime = dt.datetime.strptime(request.args.get('end_date'), "%Y-%m-%dT%H:%M:%SZ")
+                endTime = dt.datetime.strptime(request.args.get("end_date"), "%Y-%m-%dT%H:%M:%SZ")
             except ValueError:
-                return 'Invalid end_date. Should be yyyy-MM-ddThh:mm:ssZ', 400
-            if(endTime > dt.datetime.utcnow()):
-                return 'end_date cannot be in future', 400
+                return "Invalid end_date. Should be yyyy-MM-ddThh:mm:ssZ", 400
+            if endTime > dt.datetime.utcnow():
+                return "end_date cannot be in future", 400
         else:
             endTime = dt.datetime.utcnow()
-        startTime = endTime - dt.timedelta(minutes=(6*24*60)-10)
+        startTime = endTime - dt.timedelta(minutes=(6 * 24 * 60) - 10)
 
     startTime = startTime.isoformat(timespec="seconds") + "Z"
     endTime = endTime.isoformat(timespec="seconds") + "Z"
     print("Fetching weather data with values {} {} {}".format(location, startTime, endTime), flush=True)
-    obs = download_stored_query("fmi::observations::weather::multipointcoverage",
-                            args=["place=" + location,
-                                  "starttime=" + startTime,
-                                  "endtime=" + endTime,
-                                  "timeseries=True"])
+    obs = download_stored_query(
+        "fmi::observations::weather::multipointcoverage",
+        args=["place=" + location, "starttime=" + startTime, "endtime=" + endTime, "timeseries=True"],
+    )
 
-    #return jsonify(obs.data[list(obs.data.keys())[0]])
+    # return jsonify(obs.data[list(obs.data.keys())[0]])
 
     location = list(obs.data.keys())[0]
-    keys = obs.data[location]['times']
-    temperature = obs.data[location]['t2m']['values']
-    wind = obs.data[location]['ws_10min']['values']
-    windHigh = obs.data[location]['wg_10min']['values']
-    windDirection = obs.data[location]['wd_10min']['values']
-    humidity = obs.data[location]['rh']['values']
-    moistPoint = obs.data[location]['td']['values']
-    precipitation  = obs.data[location]['r_1h']['values']
-    precIntensity = obs.data[location]['ri_10min']['values']
-    snowDept = obs.data[location]['snow_aws']['values']
-    pressure = obs.data[location]['p_sea']['values']
-    visibility = obs.data[location]['vis']['values']
-    clouds = obs.data[location]['n_man']['values']
-    weather = obs.data[location]['wawa']['values']
+    keys = obs.data[location]["times"]
+    temperature = obs.data[location]["t2m"]["values"]
+    wind = obs.data[location]["ws_10min"]["values"]
+    windHigh = obs.data[location]["wg_10min"]["values"]
+    windDirection = obs.data[location]["wd_10min"]["values"]
+    humidity = obs.data[location]["rh"]["values"]
+    moistPoint = obs.data[location]["td"]["values"]
+    precipitation = obs.data[location]["r_1h"]["values"]
+    precIntensity = obs.data[location]["ri_10min"]["values"]
+    snowDept = obs.data[location]["snow_aws"]["values"]
+    pressure = obs.data[location]["p_sea"]["values"]
+    visibility = obs.data[location]["vis"]["values"]
+    clouds = obs.data[location]["n_man"]["values"]
+    weather = obs.data[location]["wawa"]["values"]
 
-    df = pd.DataFrame({'dt': keys, 't2m': temperature, 'ws_10min': wind, 'wg_10min': windHigh, 'wd_10min': windDirection, 'rh': humidity, 'td': moistPoint, 'r_1h': precipitation, 'ri_10min': precIntensity, 'snow_aws': snowDept, 'p_sea': pressure, 'vis': visibility, 'n_man': clouds, 'wawa': weather })
-    return Response(df.to_json(orient='records'), mimetype='application/json')
-
+    df = pd.DataFrame(
+        {
+            "dt": keys,
+            "t2m": temperature,
+            "ws_10min": wind,
+            "wg_10min": windHigh,
+            "wd_10min": windDirection,
+            "rh": humidity,
+            "td": moistPoint,
+            "r_1h": precipitation,
+            "ri_10min": precIntensity,
+            "snow_aws": snowDept,
+            "p_sea": pressure,
+            "vis": visibility,
+            "n_man": clouds,
+            "wawa": weather,
+        }
+    )
+    return Response(df.to_json(orient="records"), mimetype="application/json")
