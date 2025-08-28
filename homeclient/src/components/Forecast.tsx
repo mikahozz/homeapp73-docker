95e785acf544de76699ca26069a25b2856249687
Mika Ahopelto <mika.ahopelto@gmail.com>
2025-08-28 18:56:52 +0300

    Fixed the weather symbol logic

--- homeclient/src/components/Forecast.tsx
+++ homeclient/src/components/Forecast.tsx
@@ -99,9 +99,6 @@ export function Forecast() {
                   dayLabel = forecastDateTime.toFormat("EEEE").toUpperCase();
                 }
               }
-              const iconNumber = isDay
-                ? forecastitem.weather
-                : Number.parseInt(forecastitem.weather) + 100;
 
               return (
                 <React.Fragment key={forecastitem.datetime}>
@@ -121,7 +118,7 @@ export function Forecast() {
                         alt=""
                         width="55"
                         height="55"
-                        src={`/img/${iconNumber}.svg`}
+                        src={`/img/${forecastitem.weather}.svg`}
                       />
                     </td>
                     <td className="temperature-col">
